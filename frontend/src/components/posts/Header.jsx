c65358aedbb9fa70dd33c58e01195a44620cd525
Ben Matela <bmatela@afrogeek.co.za>
2022-09-10 15:05:56 +0200

    header test refactor

--- frontend/src/components/posts/Header.jsx
+++ frontend/src/components/posts/Header.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
       <div className="w-full text-black">
-        <div id="links" className="max-w-[1240px] mt-3 mx-auto grid grid-cols-2 text-center">
-          <Link to="/posts" className="hover:underline link">
+        <div id="links" data-testid={"links"} className="max-w-[1240px] mt-3 mx-auto grid grid-cols-2 text-center">
+          <Link to="/posts" data-testid={"link"} className="hover:underline link">
             LIST POSTS
           </Link>
-          <Link to="/posts/add" className="hover:underline link">
+          <Link to="/posts/add" data-testid={"link"} className="hover:underline link">
             ADD POST
           </Link>
         </div>
--- frontend/src/tests/Header.test.js
+++ frontend/src/tests/Header.test.js
@@ -1,15 +1,15 @@
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "../components/posts/Header";
 
 afterEach(cleanup);
 
 describe("Test Header component", () => {
-  it("should render", () => {
+  it("should render", async () => {
     const view = render(<BrowserRouter><Header /></BrowserRouter>);
 
     expect(view).toMatchSnapshot();
-    
-    expect(document.getElementsByClassName("link")).toHaveLength(2);
+    const links = await (await screen.findAllByTestId("link")).length;
+    expect(links).toEqual(2);
   });
 });
